a8ee4cd8823c4c2a24b84d2697d7cd54d01d396e
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-12-21 09:53:38 -0300

    Commit inicial

---
+++ src/test/java/com/douglas/developer/radio/gateway/RadioGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.developer.radio.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
